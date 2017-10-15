1c1daf16d1243bbabce96a1591eea8ba35878626
leemon <tacrapo@gmail.com>
2017-10-15 20:49:45 +0200

    small fixes

--- woocommerce-alternate-product-categories.php
+++ woocommerce-alternate-product-categories.php
@@ -16,9 +16,9 @@ class WooCommerce_Alternate_Product_Categories extends WP_Widget {
 	 */
 	function __construct() {
 		load_plugin_textdomain( 'wapc-widget', false, dirname( plugin_basename( __FILE__ ) ) );
-		$widget_ops = array('classname' => 'woocommerce widget_alternate_product_categories', 'description' => __('A WooCommerce alternate product categories widget', 'wapc-widget'));
-		$control_ops = array('width' => 400, 'height' => 350);
-		parent::__construct('wapc', __('WooCommerce Alternate Product Categories', 'wapc-widget'), $widget_ops, $control_ops);
+		$widget_ops = array( 'classname' => 'woocommerce widget_alternate_product_categories', 'description' => __( 'A WooCommerce alternate product categories widget', 'wapc-widget' ) );
+		$control_ops = array( 'width' => 400, 'height' => 350 );
+		parent::__construct( 'wapc', __( 'WooCommerce Alternate Product Categories', 'wapc-widget' ), $widget_ops, $control_ops );
 	}
 
 	/**
@@ -30,120 +30,125 @@ class WooCommerce_Alternate_Product_Categories extends WP_Widget {
 	 * @param array $instance
 	 */
 	function widget( $args, $instance ) {
-		extract($args);
-		$title = apply_filters( 'widget_title', empty($instance['title']) ? '' : $instance['title'], $instance );
+		extract( $args );
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance );
 		$count = $instance['count'] ? true : false;
 		$dropdown = $instance['dropdown'] ? true : false;
 		
 		echo $before_widget;
 		
-		if (!empty( $title )) { 
+		if ( !empty( $title ) ) { 
 			echo $before_title . $title . $after_title;
 		};
 		
-		$term = get_queried_object()->term_id;
-		$termid = get_term($term, 'product_cat' );
+		$term_id = get_queried_object()->term_id;
+		$term = get_term( $term_id, 'product_cat' );
 
-		if ($dropdown) {
+		if ( $term && !is_wp_error( $term ) ) {
+					
+			if ( $dropdown ) {
 
-			if ($termid->parent > 0) { 
-				$args = array(
-					'orderby'       => 'name', 
-					'order'         => 'ASC',
-					'hide_empty'    => true, 
-					'child_of'      => $termid->parent,
-					'show_count'    => $count ? 1 : 0,
-					'selected'      => $termid ? $termid->slug : ''
-				); 
-			
-			} else {
-				$args = array(
-					'orderby'       => 'name', 
-					'order'         => 'ASC',
-					'hide_empty'    => true, 
-					'child_of'      => $term,
-					'show_count'    => $count ? 1 : 0,
-					'selected'      => $termid ? $termid->slug : ''
-				);
-			}
-			
-			$current_product_cat = isset( $wp_query->query_vars['product_cat'] ) ? $wp_query->query_vars['product_cat'] : '';
-			$terms = get_terms( 'product_cat', $args );
-			
-			$output  = "<select name='product_cat' class='dropdown_product_cat'>";
-			$output .= '<option value="" ' . selected( $term, '', false ) . '>' . __( 'Select a category', 'woocommerce' ) . '</option>';
-			$output .= wc_walk_category_dropdown_tree( $terms, 0, $args );
-			$output .= "</select>";
-
-			echo $output;
-			
-			wc_enqueue_js( "
-				jQuery( '.dropdown_product_cat' ).change( function() {
-					if ( jQuery(this).val() != '' ) {
-						var this_page = '';
-						var home_url  = '" . esc_js( home_url( '/' ) ) . "';
-						if ( home_url.indexOf( '?' ) > 0 ) {
-							this_page = home_url + '&product_cat=' + jQuery(this).val();
-						} else {
-							this_page = home_url + '?product_cat=' + jQuery(this).val();
-						}
-						location.href = this_page;
-					}
-				});
-			" );
-			
-			
-		} else {
-			
-			echo '<ul class="product-categories">';
-
-			if ($termid->parent > 0) { 
-				$args = array(
-					'orderby'       => 'name', 
-					'order'         => 'ASC',
-					'hide_empty'    => true, 
-					'child_of'      => $termid->parent,
-				); 
-
-				$siblingcategories = get_terms( 'product_cat', $args);
-
-				foreach ($siblingcategories as $siblingcategory) { 
-					if ($siblingcategory->term_id == $term ) {
-						echo '<li class="cat-item cat-item-' . $siblingcategory->term_id . ' current-cat">';
-					} else {
-						echo '<li class="cat-item cat-item-' . $siblingcategory->term_id . '">';
-					}
+				if ( $term->parent > 0 ) { 
+					$args = array(
+						'orderby'       => 'name', 
+						'order'         => 'ASC',
+						'hide_empty'    => true, 
+						'child_of'      => $term->parent,
+						'show_count'    => $count ? 1 : 0,
+						'selected'      => $term ? $term->slug : ''
+					); 
 				
-					echo '<a href="' . get_term_link( $siblingcategory ) . '">' . $siblingcategory->name . '</a>';
-					if ($count) {
-						echo '<span class="count">(' .  $siblingcategory->count . ')</span>';
-					}
-					echo '</li>';
+				} else {
+					$args = array(
+						'orderby'       => 'name', 
+						'order'         => 'ASC',
+						'hide_empty'    => true, 
+						'child_of'      => $term_id,
+						'show_count'    => $count ? 1 : 0,
+						'selected'      => $term ? $term->slug : ''
+					);
 				}
+				
+				$current_product_cat = isset( $wp_query->query_vars['product_cat'] ) ? $wp_query->query_vars['product_cat'] : '';
+				$terms = get_terms( 'product_cat', $args );
+				
+				$output  = "<select name='product_cat' class='dropdown_product_cat'>";
+				$output .= '<option value="" ' . selected( $term_id, '', false ) . '>' . __( 'Select a category', 'woocommerce' ) . '</option>';
+				$output .= wc_walk_category_dropdown_tree( $terms, 0, $args );
+				$output .= "</select>";
 
-			} else { 
-		
-				$args = array(
-					'orderby'       => 'name', 
-					'order'         => 'ASC',
-					'hide_empty'    => true, 
-					'child_of'      => $term 
-				);
+				echo $output;
+				
+				wc_enqueue_js( "
+					jQuery( '.dropdown_product_cat' ).change( function() {
+						if ( jQuery( this ).val() != '' ) {
+							var this_page = '';
+							var home_url  = '" . esc_js( home_url( '/' ) ) . "';
+							if ( home_url.indexOf( '?' ) > 0 ) {
+								this_page = home_url + '&product_cat=' + jQuery( this ).val();
+							} else {
+								this_page = home_url + '?product_cat=' + jQuery( this ).val();
+							}
+							location.href = this_page;
+						}
+					});
+				" );
+				
+				
+			} else {
+				
+				echo '<ul class="product-categories">';
 
-				$subcategories = get_terms( 'product_cat', $args);
+				if ( $term->parent > 0 ) { 
 
-				foreach ($subcategories as $subcategory) {
-					echo '<li class="cat-item cat-item-' . $subcategory->term_id . '"><a href="' . get_term_link( $subcategory ) . '">' . $subcategory->name . '</a>';
-					if ($count) {
-						echo '<span class="count">(' . $subcategory->count . ')</span>';
+					$args = array(
+						'orderby'       => 'name', 
+						'order'         => 'ASC',
+						'hide_empty'    => true, 
+						'child_of'      => $term->parent,
+					); 
+
+					$siblingcategories = get_terms( 'product_cat', $args );
+
+					foreach ( $siblingcategories as $siblingcategory ) { 
+						if ($siblingcategory->term_id == $term_id ) {
+							echo '<li class="cat-item cat-item-' . $siblingcategory->term_id . ' current-cat">';
+						} else {
+							echo '<li class="cat-item cat-item-' . $siblingcategory->term_id . '">';
+						}
+					
+						echo '<a href="' . get_term_link( $siblingcategory ) . '">' . $siblingcategory->name . '</a>';
+						if ( $count ) {
+							echo '<span class="count">( ' .  $siblingcategory->count . ' )</span>';
+						}
+						echo '</li>';
 					}
-					echo '</li>';
-				}
 
-			}
-		
-			echo '</ul>';
+				} else { 
 			
+					$args = array(
+						'orderby'       => 'name', 
+						'order'         => 'ASC',
+						'hide_empty'    => true, 
+						'child_of'      => $term_id 
+					);
+
+					$subcategories = get_terms( 'product_cat', $args);
+
+					foreach ( $subcategories as $subcategory ) {
+						echo '<li class="cat-item cat-item-' . $subcategory->term_id . '"><a href="' . get_term_link( $subcategory ) . '">' . $subcategory->name . '</a>';
+						if ( $count ) {
+							echo '<span class="count">( ' . $subcategory->count . ' )</span>';
+						}
+						echo '</li>';
+					}
+
+				}
+			
+				echo '</ul>';
+				
+			}
+
 		}
 
 		echo $after_widget;
@@ -159,7 +164,7 @@ class WooCommerce_Alternate_Product_Categories extends WP_Widget {
 	 */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['count'] = $new_instance['count'];
 		$instance['dropdown'] = $new_instance['dropdown'];
 		return $instance;
@@ -172,26 +177,26 @@ class WooCommerce_Alternate_Product_Categories extends WP_Widget {
 	 * @param array $instance
 	 */
 	 function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'count' => 'off', 'dropdown' => 'off' ) );
-		$title = strip_tags($instance['title']);
+		$instance = wp_parse_args( ( array ) $instance, array( 'title' => '', 'count' => 'off', 'dropdown' => 'off' ) );
+		$title = strip_tags( $instance['title'] );
 		$count = $instance['count'];
 		$dropdown = $instance['dropdown'];
 		
 		?>
 			<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'wapc-widget'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'wapc-widget' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 			</p>
 			<p>
-			<input class="checkbox" type="checkbox" <?php checked($count, 'on'); ?> id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" /> 
-			<label for="<?php echo $this->get_field_id('count'); ?>"><?php _e('Show product counts', 'wapc-widget'); ?></label>
+			<input class="checkbox" type="checkbox" <?php checked( $count, 'on' ); ?> id="<?php echo $this->get_field_id( 'count' ); ?>" name="<?php echo $this->get_field_name( 'count' ); ?>" /> 
+			<label for="<?php echo $this->get_field_id( 'count' ); ?>"><?php _e( 'Show product counts', 'wapc-widget' ); ?></label>
 			</p>
 			<p>
-			<input class="checkbox" type="checkbox" <?php checked($dropdown, 'on'); ?> id="<?php echo $this->get_field_id('dropdown'); ?>" name="<?php echo $this->get_field_name('dropdown'); ?>" /> 
-			<label for="<?php echo $this->get_field_id('dropdown'); ?>"><?php _e('Show dropdown', 'wapc-widget'); ?></label>
+			<input class="checkbox" type="checkbox" <?php checked( $dropdown, 'on' ); ?> id="<?php echo $this->get_field_id( 'dropdown' ); ?>" name="<?php echo $this->get_field_name( 'dropdown' ); ?>" /> 
+			<label for="<?php echo $this->get_field_id( 'dropdown' ); ?>"><?php _e( 'Show dropdown', 'wapc-widget' ); ?></label>
 			</p>
 		<?php
 	}
 }
 
-add_action('widgets_init', create_function('', 'return register_widget("WooCommerce_Alternate_Product_Categories");'));
+add_action( 'widgets_init', create_function( '', 'return register_widget( "WooCommerce_Alternate_Product_Categories" );' ) );
