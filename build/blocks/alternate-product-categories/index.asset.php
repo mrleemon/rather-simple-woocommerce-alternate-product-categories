bf8a3216c3984572633ad40e67ed24b2e2437be1
leemon <tacrapo@gmail.com>
2023-07-23 01:08:41 +0200

    Add missing imports

--- build/blocks/alternate-product-categories/index.asset.php
+++ build/blocks/alternate-product-categories/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'def60d1557d832c550ab');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bb31f95a7d97f27ebd5e');
--- src/blocks/alternate-product-categories/index.js
+++ src/blocks/alternate-product-categories/index.js
@@ -3,6 +3,8 @@
  */
 import { __ } from '@wordpress/i18n';
 import {
+    SVG,
+    Path,
     Disabled,
     PanelBody,
     ToggleControl,
