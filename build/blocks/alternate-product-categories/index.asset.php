a7cf4ac62479c4571063b4cde36df0abdddb35f3
leemon <tacrapo@gmail.com>
2023-07-23 01:04:25 +0200

    Add custom block icon

--- build/blocks/alternate-product-categories/index.asset.php
+++ build/blocks/alternate-product-categories/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '5c372d62d7b757c8c8d5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'def60d1557d832c550ab');
--- src/blocks/alternate-product-categories/index.js
+++ src/blocks/alternate-product-categories/index.js
@@ -23,6 +23,13 @@ const { name } = metadata;
 
 const settings = {
 
+    icon: {
+        src: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		    <Path d="M3 6h11v1.5H3V6Zm3.5 5.5h11V13h-11v-1.5ZM21 17H10v1.5h11V17Z" />
+	        </SVG>,
+        foreground: '#ff8a00'
+    },
+
     edit: (props) => {
         const attributes = props.attributes;
 
