6a5fbd111e6c526c84e009ab5b2e2a3b9602a1b0
leemon <tacrapo@gmail.com>
2024-11-22 11:34:56 +0100

    Rebuild

--- build/blocks/alternate-product-categories/index.asset.php
+++ build/blocks/alternate-product-categories/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'dab0b6d8315c42f7b35f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'cfa15245d5e0800b6b0b');
