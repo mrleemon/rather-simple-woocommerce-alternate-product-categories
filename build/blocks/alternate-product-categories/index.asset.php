a0302874afe6b6685001e7dbcfca35a44634a389
leemon <tacrapo@gmail.com>
2025-04-15 15:40:52 +0200

    Fix import

--- build/blocks/alternate-product-categories/index.asset.php
+++ build/blocks/alternate-product-categories/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '3a8cfa7edc75903f5791');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '81b68ab7a17ea11b80bb');
